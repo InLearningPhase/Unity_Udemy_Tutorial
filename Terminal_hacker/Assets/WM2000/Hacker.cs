5f3a4642c80088b41e0eddecf217cc6ccdc8244b
Tanesh Gupta <40913077+InLearningPhase@users.noreply.github.com>
2020-02-25 07:44:41 +0530

    Update hacker.cs

--- Terminal_hacker/Assets/WM2000/Hacker.cs
+++ Terminal_hacker/Assets/WM2000/Hacker.cs
@@ -7,6 +7,7 @@ public class Hacker : MonoBehaviour {
     int level;
     enum Screen {MainMenu, Password, Win};
     Screen currentScreen;
+    string password;
     
     // Start is called before the first frame update
     void Start()
@@ -34,8 +35,11 @@ public class Hacker : MonoBehaviour {
        {
             RunMainMenu(input);
 	   }
-       
-   }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
 
    void RunMainMenu(string input)
    {
@@ -43,11 +47,13 @@ public class Hacker : MonoBehaviour {
        {
             level = 1;
             StartGame();
+            password = "qwerty";
 	   }
        else if (input == "2")
        {
             level = 2;
             StartGame();
+            password = "qq";
 	   }
        else
        {
@@ -60,5 +66,17 @@ public class Hacker : MonoBehaviour {
        currentScreen = Screen.Password;
        Terminal.WriteLine("You have choosen level " + level);
        Terminal.WriteLine("Please enter your password: ");
+   }
+
+   void CheckPassword(string input)
+   {
+        if (input == password)
+        {
+            Terminal.WriteLine("Password Confirmed");
+        }
+        else
+        {
+            Terminal.WriteLine("Wrong Password");
+        }
    }
 }
